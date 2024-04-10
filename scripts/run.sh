f0f8d062f9e1ff20202480cad11a45bc7ac5b373
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2024-04-10 10:58:52 +0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,13 +6,13 @@
 #SBATCH --error="/media/nhdang/Vy_Cat/newPCMDM/scripts/log_out/training/dhms.err"
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH --mem=60G
-#SBATCH --cpus-per-task=40
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=8
 #SBATCH -w gpu05
 
 source /media/nhdang/hieunmt/miniconda3/etc/profile.d/conda.sh
 
-cd /media/nhdang/Vy_Cat/newPCMDM
+cd /media/nhdang/Vy_Cat/dhms
 
 conda activate dhms
 
