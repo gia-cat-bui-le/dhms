4313231a6c887a1c084cd9fbf91d179405e557b5
trgvy23 <truongthuytuongvy2301@gmail.com>
2024-04-14 11:08:37 +0700

    Update metrics_new.py

--- evaluation/metrics_new.py
+++ evaluation/metrics_new.py
@@ -162,7 +162,7 @@ def calc_and_save_feats(root):
         if os.path.isdir(os.path.join(root, pkl)):
             continue
             
-        joint3d = np.load(os.path.join(root, pkl), allow_pickle=True)['full_pose'][:225,:].reshape([225, -1])
+        joint3d = np.load(os.path.join(root, pkl), allow_pickle=True)['full_pose'][:150,:].reshape([150, -1])
         # print(extract_manual_features(joint3d.reshape(-1, 24, 3)))
         roott = joint3d[:1, :3]  # the root Tx72 (Tx(24x3))
         # print(roott)
