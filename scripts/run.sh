05aed31f4ca791e091ecc9f37d44050155953fb4
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2024-05-05 18:54:22 +0700

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=job_baseline
 #SBATCH --gpus=1             # total number of GPUs
-#SBATCH --output="/media/nhdang/Vy_Cat/dhms-baseline/scripts/log_out/training_baseline/dhms.out"
-#SBATCH --error="/media/nhdang/Vy_Cat/dhms-baseline/scripts/log_out/training_baseline/dhms.err"
+#SBATCH --output="/media/nhdang/Vy_Cat/dhms-baseline/scripts/log_out/training_baseline_1.0/dhms.out"
+#SBATCH --error="/media/nhdang/Vy_Cat/dhms-baseline/scripts/log_out/training_baseline_1.0/dhms.err"
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=16
@@ -12,6 +12,6 @@ source /media/nhdang/hieunmt/miniconda3/etc/profile.d/conda.sh
 
 conda activate dhms
 
-cd /media/nhdang/Vy_Cat/dhms
+cd /media/nhdang/Vy_Cat/dhms-baseline
 
-python3 train_diffusion.py --save_dir /raid/nhdang/Vy/dhms/pcmdm_baseline --dataset aistpp --eval_during_training --refine --inference_dir /raid/nhdang/Vy/data/evaluation_baseline
+python3 train_diffusion.py --save_dir /raid/nhdang/Vy/dhms/pcmdm_baseline_1.0 --dataset aistpp --eval_during_training --refine --inference_dir /raid/nhdang/Vy/data/evaluation_baseline_1.0
