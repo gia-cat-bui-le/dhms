8fd0a7a306e477cc8a210f45abc896f9d9f58622
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2024-05-14 21:57:07 +0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=job_baseline
+#SBATCH --job-name=baseline-inbetweening
 #SBATCH --gpus=1             # total number of GPUs
-#SBATCH --output="/media/nhdang/Vy_Cat/dhms-baseline/scripts/log_out/training_baseline_1.0/dhms.out"
-#SBATCH --error="/media/nhdang/Vy_Cat/dhms-baseline/scripts/log_out/training_baseline_1.0/dhms.err"
+#SBATCH --output="/media/nhdang/Vy_Cat/baseline-inbetweening/scripts/log_out/training_baseline_inbetween/dhms.out"
+#SBATCH --error="/media/nhdang/Vy_Cat/baseline-inbetweening/scripts/log_out/training_baseline_inbetween/dhms.err"
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=16
@@ -12,6 +12,6 @@ source /media/nhdang/hieunmt/miniconda3/etc/profile.d/conda.sh
 
 conda activate dhms
 
-cd /media/nhdang/Vy_Cat/dhms-baseline
+cd /media/nhdang/Vy_Cat/baseline-inbetweening
 
-python3 train_diffusion.py --save_dir /raid/nhdang/Vy/dhms/pcmdm_baseline_1.0 --dataset aistpp --eval_during_training --inference_dir /raid/nhdang/Vy/data/evaluation_baseline_1.0
+python3 train_diffusion.py --save_dir /raid/nhdang/Vy/dhms/baseline-inbetweening --dataset aistpp --eval_during_training --inference_dir /raid/nhdang/Vy/databaseline-inbetweening
