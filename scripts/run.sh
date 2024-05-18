4602adbc95b041b87cad65cd29ae134780a91869
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2024-05-18 23:40:38 +0700

    return back normalizer

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=baseline-inbetweening
 #SBATCH --gpus=1             # total number of GPUs
-#SBATCH --output="/media/nhdang/Vy_Cat/baseline-inbetweening/scripts/log_out/training_baseline_inbetween_no_normalize/dhms.out"
-#SBATCH --error="/media/nhdang/Vy_Cat/baseline-inbetweening/scripts/log_out/training_baseline_inbetween_no_normalize/dhms.err"
+#SBATCH --output="/media/nhdang/Vy_Cat/baseline-inbetweening/scripts/log_out/training_baseline_inbetween/dhms.out"
+#SBATCH --error="/media/nhdang/Vy_Cat/baseline-inbetweening/scripts/log_out/training_baseline_inbetween/dhms.err"
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=16
@@ -14,4 +14,4 @@ conda activate dhms
 
 cd /media/nhdang/Vy_Cat/baseline-inbetweening
 
-python3 train_diffusion.py --save_dir /raid/nhdang/Vy/dhms/baseline-inbetweening_no_normalize --dataset aistpp --eval_during_training --inference_dir /raid/nhdang/Vy/databaseline-inbetweening_no_normalize
+python3 train_diffusion.py --save_dir /raid/nhdang/Vy/dhms/baseline-inbetweening --dataset aistpp --eval_during_training --inference_dir /raid/nhdang/Vy/databaseline-inbetweening
