d5f91d834462693102ae4df28f6496605736bd29
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2024-05-18 17:23:03 +0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=baseline-sinmdm
 #SBATCH --gpus=1             # total number of GPUs
-#SBATCH --output="/media/nhdang/Vy_Cat/baseline-sinmdm/scripts/log_out/training_baseline_inbetween/dhms.out"
-#SBATCH --error="/media/nhdang/Vy_Cat/baseline-sinmdm/scripts/log_out/training_baseline_inbetween/dhms.err"
+#SBATCH --output="/media/nhdang/Vy_Cat/baseline-sinmdm/scripts/log_out/training_baseline_sinmdm/dhms.out"
+#SBATCH --error="/media/nhdang/Vy_Cat/baseline-sinmdm/scripts/log_out/training_baseline_sinmdm/dhms.err"
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=16
