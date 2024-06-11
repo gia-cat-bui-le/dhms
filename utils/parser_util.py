f559b52da80e18ad8a7fd353e7c7ac5d1d27dcd7
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2024-06-11 09:34:35 +0700

    Update parser_util.py

--- utils/parser_util.py
+++ utils/parser_util.py
@@ -157,9 +157,9 @@ def add_training_options(parser):
                        help="If True, will enable to use an already existing save_dir.")
     group.add_argument("--train_platform_type", default='NoPlatform', choices=['NoPlatform', 'ClearmlPlatform', 'TensorboardPlatform'], type=str,
                        help="Choose platform to log results. NoPlatform means no logging.")
-    group.add_argument("--lr", default=2e-4, type=float, help="Learning rate.")
-    group.add_argument("--weight_decay", default=0.0001, type=float, help="Optimizer weight decay.")
-    group.add_argument("--lr_anneal_steps", default=200000, type=int, help="Number of learning rate anneal steps.")
+    group.add_argument("--lr", default=1e-4, type=float, help="Learning rate.")
+    group.add_argument("--weight_decay", default=0.01, type=float, help="Optimizer weight decay.")
+    group.add_argument("--lr_anneal_steps", default=300000, type=int, help="Number of learning rate anneal steps.")
     group.add_argument("--eval_batch_size", default=32, type=int,
                        help="Batch size during evaluation loop. Do not change this unless you know what you are doing. "
                             "T2m precision calculation is based on fixed batch size 32.")
