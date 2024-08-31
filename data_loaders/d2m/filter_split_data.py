4cdd7b04098a1091c3e301fb5da862a032a6f60c
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2024-08-31 16:31:08 +0700

    Update filter_split_data.py

--- data_loaders/d2m/filter_split_data.py
+++ data_loaders/d2m/filter_split_data.py
@@ -40,12 +40,9 @@ def get_train_test_list_finedance(dataset_path):
             if one not in val_list:
                 train_list.append(one)
 
-    data_split["train"] = train_list
-    data_split["test"] = test_list
-    data_split["val"] = val_list
-    data_split["ignore"] =  ["116", "117", "118", "119", "120", "121", "122", "123", "202"]
+    filter_list = ["116", "117", "118", "119", "120", "121", "122", "123", "202"]
     
-    return train_list, test_list, val_list
+    return filter_list, train_list, test_list, val_list
 
 def split_data(dataset_path):
     filter_list, train_list, test_list = get_train_test_list(dataset_path)
@@ -69,12 +66,14 @@ def split_data(dataset_path):
             shutil.copyfile(wav, f"{dataset_path}/{split_name}/wavs/{sequence}.wav")
             
 def split_data_finedance(dataset_path):
-    train_list, test_list, val_list = get_train_test_list_finedance(dataset_path)
+    filter_list, train_list, test_list, val_list = get_train_test_list_finedance(dataset_path)
     # train - test split
     for split_list, split_name in zip([train_list, test_list], ["train", "test"]):
         Path(f"{dataset_path}/{split_name}/motions").mkdir(parents=True, exist_ok=True)
         Path(f"{dataset_path}/{split_name}/wavs").mkdir(parents=True, exist_ok=True)
         for sequence in split_list:
+            if sequence in filter_list:
+                continue
             motion = f"{dataset_path}/motion_fea319/{sequence}.npy"
             wav = f"{dataset_path}/music_npy/{sequence}.npy"
             assert os.path.isfile(motion)
